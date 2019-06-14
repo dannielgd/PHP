316c8e499e6e8575aac5404e80c09aa085921f18
Danniel <dannielgd@gmail.com>
2019-06-14 16:54:45 -0300

    Bug Fixed

--- Projeto_01/painel/js/main.js
+++ Projeto_01/painel/js/main.js
@@ -28,7 +28,7 @@ $(function () {
              });
              
              if(windowSize > 768)
-                 $('.content, header').css({width : 'calc(100% - 250px)'});
+                 $('.content, header').css({width : 'calc(100% - 300px)'});
                  $('.content, header').animate({left : targetSizeMenu + 'px'}, function() {
                      open = true;
                  });   
@@ -37,6 +37,7 @@ $(function () {
     
     $(window).resize(function() {
         windowSize = $(window)[0].innerWidth;
+        targetSizeMenu = (windowSize <= 400) ? 200 : 300;
         
         if(windowSize <= 768) {
             $('.menu').css({width : '0'}).css({padding : '0'});
@@ -44,8 +45,8 @@ $(function () {
             open = false;
         } else {
             open = true;
-            $('.content, header').css({width : 'calc(100% - 250px)'}).css({left : '250px'});
-            $('.menu').css({width : '100%'}).css({padding : '10px 0'});
+            $('.content, header').css({width : 'calc(100% - 300px)'}).css({left : targetSizeMenu + 'px'});
+            $('.menu').css({width : targetSizeMenu + 'px'}).css({padding : '10px 0'});
         }
     })
 })
